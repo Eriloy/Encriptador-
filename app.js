57865aace1cc137fddd0b4e66123c153e60b73af
Eriloy <emyliwi@gmail.com>
2024-07-16 23:07:06 -0400

    Añadir archivo .gitignore basado en la plantilla de GitHub y configuraciones de Visual Studio Code

---
+++ app.js
@@ -0,0 +1,21 @@
+function encryptText(text) {
+    const encryptingMap = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+    return text.replace(/enter|imes|ai|ober|ufat/g ,match => encryptingMap[match]);
+    }
+
+function decryptText(text) {
+    const decryptingMap = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+    return text.replace(/enter|imes|ai|ober|ufat/g ,match => decryptingMap[match]);
+    }
