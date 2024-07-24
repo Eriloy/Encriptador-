054dbabf4c0c6fbd133801b8c69c3422b6afd240
Eriloy <emyliwi@gmail.com>
2024-07-24 02:39:10 -0400

    Añadir divs columna derecha y columna izquiera, reorganizar contenido en columnas

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
+function myFunction() {
+    console.log("myFunction called");
+    var element = document.querySelector ('.general-container');
+    element.classList.toggle("dark-mode");
+}
+
+const encryptButton = document.getElementById('encryptButton');
+const decryptButton = document.getElementById('decryptButton');
+const encryptorOutput = document.querySelector('.encryptor__output');
+
+
+function setTextAndShowOutput(text) {
+    encryptorOutput.querySelector('.textarea--output').value = text;
+    encryptorOutput.classList.add('show-output');
+}
+
 function encryptText(text) {
     const encryptingMap = {
-        'e': 'enter',
-        'i': 'imes',
-        'a': 'ai',
-        'o': 'ober',
-        'u': 'ufat'
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
     };
-    return text.replace(/enter|imes|ai|ober|ufat/g ,match => encryptingMap[match]);
-    }
+    const encryptedText = text.replace(/enter|imes|ai|ober|ufat/g ,match => encryptingMap[match]);
+    return encryptedText; // Return the encrypted text
+}
 
 function decryptText(text) {
     const decryptingMap = {
-        'enter': 'e',
-        'imes': 'i',
-        'ai': 'a',
-        'ober': 'o',
-        'ufat': 'u'
+    'enter': 'e',
+    'imes': 'i',
+    'ai': 'a',
+    'ober': 'o',
+    'ufat': 'u'
     };
-    return text.replace(/enter|imes|ai|ober|ufat/g ,match => decryptingMap[match]);
-    }
+    const decryptedText = text.replace(/enter|imes|ai|ober|ufat/g ,match => decryptingMap[match]);
+    return decryptedText; // Return the decrypted text
+}
+
+    encryptorOutput.classList.add('show-output');
+    
+
+    encryptButton.addEventListener('click', () => {
+
+        console.log('Encrypt button clicked!');
+        encryptText(document.getElementById('inputText').value);
+});
+
+    decryptButton.addEventListener('click', () => {
+    console.log('Decrypt button clicked!');
+    decryptText(document.getElementById('inputText').value); 
+    });
+    
