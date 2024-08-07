47dadb948954f61894631039550d843823dc6c70
Eriloy <emyliwi@gmail.com>
2024-08-07 06:37:16 -0400

    definir variables css

--- app.js
+++ app.js
@@ -9,12 +9,43 @@ const decryptButton = document.getElementById('decryptButton');
 const encryptorOutput = document.querySelector('.encryptor__output');
 
 
-function setTextAndShowOutput(text) {
+
+/*function setTextAndShowOutput(text) {
     encryptorOutput.querySelector('.textarea--output').value = text;
     encryptorOutput.classList.add('show-output');
+}*/
+encryptButton.addEventListener('click', () => {
+    const inputText = document.getElementById('  inputText').value;
+    const encryptedText = inputText.replace(/enter|imes|ai|ober|ufat/g ,match => ({
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+    })[match]);
+    // Update the output with the encrypted text (replace with your logic)
+    console.log(encryptedText); 
+});
+
+
+function decryptText(text) {
+    const decryptingMap = {
+    'enter': 'e',
+    'imes': 'i',
+    'ai': 'a',
+    'ober': 'o',
+    'ufat': 'u'
+    };
+    const decryptedText = text.replace(/enter|imes|ai|ober|ufat/g ,match => decryptingMap[match]);
+    return decryptedText; // Return the decrypted text
 }
 
-function encryptText(text) {
+/*function setTextAndShowOutput(text) {
+    encryptorOutput.querySelector('.textarea--output').value = text;
+    encryptorOutput.classList.add('show-output');
+}*/
+
+/*function encryptText(text) {
     const encryptingMap = {
     'e': 'enter',
     'i': 'imes',
@@ -51,4 +82,4 @@ function decryptText(text) {
     console.log('Decrypt button clicked!');
     decryptText(document.getElementById('inputText').value); 
     });
-    
+    */
